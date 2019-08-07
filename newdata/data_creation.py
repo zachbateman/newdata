c72fe7dfee58897026d05e6605b4891511fce2ad
Zach Bateman <zkbateman@gmail.com>
2019-08-07 16:38:09 -0500

    Improve list of possible functions and add function-picker, add fuzzify func, add create_data algorithm

--- newdata/data_creation.py
+++ newdata/data_creation.py
@@ -13,12 +13,9 @@ param_funcs = [
     lambda x: math.log(x),
     lambda x: math.log(x, 5),
     lambda x: math.log(x, 10),
-    lambda x: math.pow(x, 0.1),
-    lambda x: math.pow(x, 0.4),
-    lambda x: math.pow(x, 0.75),
-    lambda x: math.pow(x, 1.5),
-    lambda x: math.pow(x, 2.0),
-    lambda x: math.pow(x, 3.0),
+    lambda x: math.pow(x, 1),
+    lambda x: math.pow(x, 2),
+    lambda x: math.pow(x, 3),
     lambda x: math.sqrt(x),
     lambda x: math.acos(x),
     lambda x: math.asin(x),
@@ -44,19 +41,46 @@ param_funcs = [
     lambda x: 0.75 * x,
     lambda x: random.random(),
     ]
+negative_param_funcs = [lambda x: -f(x) for f in param_funcs]
+param_funcs = param_funcs + negative_param_funcs
 
+def rand_func():
+    if random.random() < 0.5:
+        return random.choice(param_funcs)
+    else:  # combine two functions to create a new function
+        f1 = random.choice(param_funcs)
+        f2 = random.choice(param_funcs)
+        return lambda x: f2(f1(x))
+
+
+def fuzzify(x, factor: float=0.5) -> float:
+    '''
+    Randomly change given number a bit to add noize/fuzz to data.
+    factor is float [0 < factor < 1] that adjusts how much fuzz.
+    '''
+    return x * (random.random() + 0.5) ** (factor + 0.1)
 
 
 class DataCreator():
 
-    def __init__(self,) -> None:
-        self.data = [{}, {}, ...]
+    def __init__(self, num_params: int=10, num_samples: int=100) -> None:
+        self.data = self.create_data(num_params, num_samples)  # [{}, {}, ...]
 
 
     def create_data(self, num_params: int=10, num_samples: int=100) -> List[Dict[str, float]]:
         '''Creates a new data set.'''
-        # create target parameter
+        # create initial data set structure with target values
+        target_func = rand_func()
+        min_initial_target = -random.random() * 10
+        max_initial_target = random.random() * 10
+        initial_values = [random.uniform(min_initial_target, max_initial_target) for _ in range(num_samples)]
+        target_values = [fuzzify(target_func(x)) for x in initial_values]
+        self.data = [{'Target': x} for x in target_values]
 
         # create associated parameters
-
-        pass
+        for i in range(1, num_params + 1):
+            param = f'Param_{i}'
+            fuzz_factor = random.random()
+            param_func = rand_func()
+            for index, d in enumerate(self.data):
+                d[param] = fuzzify(param_func(d['target']), fuzz_factor)
