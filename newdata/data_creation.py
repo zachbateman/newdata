13c67bbcffcadd2b1c73279ccaaf46ca71df2d5a
Zach Bateman <zkbateman@gmail.com>
2019-08-07 16:59:42 -0500

    Add ability to save data set as usable Python module; remove troublesome param_funcs

--- newdata/data_creation.py
+++ newdata/data_creation.py
@@ -1,41 +1,34 @@
 '''
 Python module for creating synthetic data sets.
 '''
+import os
 import math
 import random
 from typing import List, Dict
 
 
 param_funcs = [
-    lambda x: math.factorial(x),
+    lambda x: math.factorial(x // 1),
     lambda x: math.frexp(x)[0],
-    lambda x: math.exp(x),
-    lambda x: math.log(x),
-    lambda x: math.log(x, 5),
-    lambda x: math.log(x, 10),
+    lambda x: math.log(abs(x)),
+    lambda x: math.log(abs(x), 5),
+    lambda x: math.log(abs(x), 10),
     lambda x: math.pow(x, 1),
     lambda x: math.pow(x, 2),
     lambda x: math.pow(x, 3),
-    lambda x: math.sqrt(x),
-    lambda x: math.acos(x),
-    lambda x: math.asin(x),
+    lambda x: math.sqrt(abs(x)),
     lambda x: math.atan(x),
     lambda x: math.cos(x),
     lambda x: math.sin(x),
     lambda x: math.tan(x),
-    lambda x: math.acosh(x),
-    lambda x: math.asinh(x),
-    lambda x: math.atanh(x),
-    lambda x: math.cosh(x),
-    lambda x: math.sinh(x),
-    lambda x: math.tanh(x),
     lambda x: math.erf(x),
     lambda x: math.erfc(x),
     lambda x: math.gamma(x),
     lambda x: math.lgamma(x),
     lambda x: x + (random.random() - 0.5) * x,
     lambda x: 1 / x,
-    lambda x: random() * 5 * x,
+    lambda x: random.random() * 5 * x,
+    lambda x: x ** random.random(),
     lambda x: 0.25 * x,
     lambda x: 0.5 * x,
     lambda x: 0.75 * x,
@@ -64,6 +57,8 @@ def fuzzify(x, factor: float=0.5) -> float:
 class DataCreator():
 
     def __init__(self, num_params: int=10, num_samples: int=100) -> None:
+        self.num_params = num_params
+        self.num_samples = num_samples
         self.data = self.create_data(num_params, num_samples)  # [{}, {}, ...]
 
 
@@ -75,12 +70,33 @@ class DataCreator():
         max_initial_target = random.random() * 10
         initial_values = [random.uniform(min_initial_target, max_initial_target) for _ in range(num_samples)]
         target_values = [fuzzify(target_func(x)) for x in initial_values]
-        self.data = [{'Target': x} for x in target_values]
+        data = [{'Target': x} for x in target_values]
 
         # create associated parameters
         for i in range(1, num_params + 1):
             param = f'Param_{i}'
             fuzz_factor = random.random()
             param_func = rand_func()
-            for index, d in enumerate(self.data):
-                d[param] = fuzzify(param_func(d['target']), fuzz_factor)
+            for index, d in enumerate(data):
+                d[param] = fuzzify(param_func(d['Target']), fuzz_factor)
+
+        return data
+
+
+    def save_data_as_pymodule(self, module_name='newdata_set.py') -> None:
+        '''
+        Create a string which creates a Python module with
+        self's created data as an importable list.
+        '''
+        s = "'''\nPython module with a synthetic data set created by newdata.\n"
+        s += f"Number of Parameters: {self.num_params}\n"
+        s += f"Number of Samples: {self.num_samples}\n'''\n\n"
+
+        s += f"data = [\n"
+        for d in self.data:
+            s += f"  {str(d)},\n"
+        s += "  ]\n"
+
+        with open(os.path.join('.', module_name), 'w') as f:
+            f.write(s)
+        print(f'New synthetic data saved to {module_name}!')
